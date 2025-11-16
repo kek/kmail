9f52560f6de0cf5f42058a70a8e531fa4eafafaa
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Format

--- src/repo.erl
+++ src/repo.erl
@@ -11,8 +11,7 @@
 %% API
 
 start_link(Bucket) ->
-   gen_server:start_link(repo, [Bucket], []).
-
+    gen_server:start_link(repo, [Bucket], []).
 
 store(Pid, Key, Value) ->
     gen_server:call(Pid, {store, Key, Value}).
@@ -26,21 +25,21 @@ init([Bucket]) ->
     {ok, Conn} = riakc_pb_socket:start("127.0.0.1", 8087),
     {ok, {Conn, Bucket}}.
 
-handle_call({store, Key, Value},_From,{Riak, Bucket} = State) ->
+handle_call({store, Key, Value}, _From, {Riak, Bucket} = State) ->
     Object = riakc_obj:new(Bucket, term_to_binary(Key), Value),
     riakc_pb_socket:put(Riak, Object),
     {reply, ok, State};
-
 handle_call({retrieve, Key}, _From, {Pid, MyBucket} = State) ->
-    Result = case riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)) of
-        {ok, Fetched} ->
-            Binary = riakc_obj:get_value(Fetched),
-            Value = binary_to_term(Binary),
-            {ok, Value};
-        {error, notfound} ->
-            {error, notfound}
-    end,
+    Result =
+        case riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)) of
+            {ok, Fetched} ->
+                Binary = riakc_obj:get_value(Fetched),
+                Value = binary_to_term(Binary),
+                {ok, Value};
+            {error, notfound} ->
+                {error, notfound}
+        end,
     {reply, Result, State}.
 
-handle_cast(_Request,_State) ->
+handle_cast(_Request, _State) ->
     erlang:error(not_implemented).
