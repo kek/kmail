ebff41aca4619007799f90814d5c44f64ede8df7
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Format

--- src/web_server.erl
+++ src/web_server.erl
@@ -14,7 +14,7 @@ handle('GET' = _Method, [] = _Path, _Req) ->
     StatusCode = ok,
     Data = #{},
     Template = "index.html",
-    Body = render_template(Template,Data),
+    Body = render_template(Template, Data),
     Headers = [{"Content-Type", "text/html; charset=utf-8"}],
     {StatusCode, Headers, Body};
 handle('GET', [~"share", _ID], _Req) ->
@@ -23,13 +23,17 @@ handle(_Method, _Path, _Req) ->
     {404, [], ~"Not Found"}.
 
 handle_event(request_error, [Req, Error, Stacktrace], _Args) ->
-    io:format("*** ~p error ***~n~nHeaders:~n~s~nStacktrace:~n~p~n~n", [Error, printable_headers(Req), Stacktrace]),
+    io:format("*** ~p error ***~n~nHeaders:~n~s~nStacktrace:~n~p~n~n", [
+        Error, printable_headers(Req), Stacktrace
+    ]),
     ok;
-handle_event(request_complete,
-    [Req, Status, _Headers, _ResponseBody, {_Timers, _Lengths}], _) ->
+handle_event(
+    request_complete,
+    [Req, Status, _Headers, _ResponseBody, {_Timers, _Lengths}],
+    _
+) ->
     log_result(Req, Status),
     ok;
-
 handle_event(request_closed, _Data, _Args) ->
     ok;
 handle_event(Event, Data, Args) ->
@@ -43,7 +47,7 @@ human_time() ->
 path_join(Path) ->
     binary:join([~"/" | Path], ~"/").
 
-render_template(Template,Params) ->
+render_template(Template, Params) ->
     TemplateBase = ~"src/templates/",
     TemplateBinary = list_to_binary(Template),
     TemplatePath = <<TemplateBase/binary, TemplateBinary/binary>>,
