f6aa48d89e63ea2fb8dc3c0f502bd2268498ed95
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    integration test

--- test/integration_test.erl
+++ test/integration_test.erl
@@ -16,9 +16,25 @@ consumer_test_() ->
         [
             {"Sending content from sender to recipient", fun() ->
                 % Register consumer
+                {201, ConsumerBody, _} = test_helpers:http_post("/consumer", <<>>),
+                #{~"id" := ID, ~"password" := _Password} = json:decode(ConsumerBody),
+
                 % Use sender API to send
-                % Use consumer API to receive
-                ?assert(true)
+                SendContentPath = binary_to_list(<<"/share/nonpayable/from/kalle/to/", ID/binary>>),
+                {201, _Body, _Headers} =
+                    test_helpers:http_post(
+                        SendContentPath,
+                        ~"Hello",
+                        [{"Content-Type", "text/plain"}]
+                    ),
+
+                % Use consumer API to get list of packages
+                GetSharesPath = binary_to_list(<<"/share/", ID/binary>>),
+                {200, ShareList, _RespHeaders1} = test_helpers:http_get(GetSharesPath),
+                ?assertNotEqual([], json:decode(ShareList))
+
+            % TODO: Validate that we use the correct password.
+            % TODO: Get the first available package and assert that says "Hello".
             end}
         ]
     end}.
