f1b5d7692afc8289d4eb52d41d2aa801f7e5a88d
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Make repo a server

--- src/repo.erl
+++ src/repo.erl
@@ -2,25 +2,45 @@
 
 -export([store/3]).
 -export([retrieve/2]).
--export([start/0]).
+-export([start_link/1]).
 
-start() ->
-    {ok, 1}.
+-export([init/1, handle_call/3, handle_cast/2]).
 
-store(_, Key, Value) ->
-    {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087),
-    MyBucket = ~"kmails",
-    Object = riakc_obj:new(MyBucket, term_to_binary(Key), Value),
-    riakc_pb_socket:put(Pid, Object).
+-behaviour(gen_server).
 
-retrieve(_, Key) ->
-    {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087),
-    MyBucket = ~"kmails",
-    case riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)) of
+%% API
+
+start_link(Bucket) ->
+   gen_server:start_link(repo, [Bucket], []).
+
+
+store(Pid, Key, Value) ->
+    gen_server:call(Pid, {store, Key, Value}).
+
+retrieve(Pid, Key) ->
+    gen_server:call(Pid, {retrieve, Key}).
+
+%% Callbacks for `gen_server`
+
+init([Bucket]) ->
+    {ok, Conn} = riakc_pb_socket:start("127.0.0.1", 8087),
+    {ok, {Conn, Bucket}}.
+
+handle_call({store, Key, Value},_From,{Riak, Bucket} = State) ->
+    Object = riakc_obj:new(Bucket, term_to_binary(Key), Value),
+    riakc_pb_socket:put(Riak, Object),
+    {reply, ok, State};
+
+handle_call({retrieve, Key}, _From, {Pid, MyBucket} = State) ->
+    Result = case riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)) of
         {ok, Fetched} ->
             Binary = riakc_obj:get_value(Fetched),
             Value = binary_to_term(Binary),
             {ok, Value};
         {error, notfound} ->
             {error, notfound}
-    end.
+    end,
+    {reply, Result, State}.
+
+handle_cast(_Request,_State) ->
+    erlang:error(not_implemented).
--- test/kmail_test.erl
+++ test/kmail_test.erl
@@ -2,11 +2,11 @@
 -module(kmail_test).
 
 'when storing a value, it can be retrieved later   _test'() ->
-    {ok, Repo} = repo:start(),
+    {ok, Repo} = repo:start_link(~"kmail_test"),
     ok = repo:store(Repo, "Hey", "There"),
     {ok, Value} = repo:retrieve(Repo, "Hey"),
     ?assertEqual("There", Value).
 
 'when trying to retrieve a value that doesn\'t exist, we get an error   _test'() ->
-    {ok, Repo} = repo:start(),
+    {ok, Repo} = repo:start_link(~"kmail_test"),
     ?assertEqual({error, notfound}, repo:retrieve(Repo, "nonexistent key")).
