0ab38962a38dc75c09dfcdef3814d54421c1c4f2
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Add logging to web server

--- src/web_server.erl
+++ src/web_server.erl
@@ -12,15 +12,42 @@ handle(Req, _Args) ->
 
 handle('GET' = _Method, [] = _Path, _Req) ->
     StatusCode = ok,
-    Headers = [{"Content-Type", "text/plain; charset=utf-8"}],
-    {ok, Bin} = file:read_file("src/templates/index.html"),
     Data = #{},
-    Body = bbmustache:render(Bin, Data),
+    Template = "index.html",
+    Body = render_template(Template,Data),
+    Headers = [{"Content-Type", "text/html; charset=utf-8"}],
     {StatusCode, Headers, Body};
 handle('GET', [~"share", _ID], _Req) ->
     {200, [], ~"[]"};
 handle(_Method, _Path, _Req) ->
     {404, [], ~"Not Found"}.
 
-handle_event(_Event, _Data, _Args) ->
+handle_event(request_error, Data, Args) ->
+    io:format("*** Request error ***\n\nData: ~p\nArgs: ~p\n\n", [Data, Args]),
+    ok;
+handle_event(request_complete,
+    [Req, Status, _Headers, _ResponseBody, {_Timers, _Lengths}], _) ->
+    Method = Req#req.method,
+    Path = path_join(Req#req.path),
+    io:format("~s ~s ~s -> ~p\n\n", [human_time(), Method, Path, Status]),
+    ok;
+handle_event(request_closed, _Data, _Args) ->
+    ok;
+handle_event(Event, Data, Args) ->
+    io:format("*** Unknown event ~p ***\n\nData: ~p\nArgs: ~p\n\n", [Event, Data, Args]),
     ok.
+
+human_time() ->
+    {H, M, S} = time(),
+    io_lib:format("~.2.0w:~.2.0w:~.2.0w", [H, M, S]).
+
+path_join(Path) ->
+    binary:join([~"/" | Path], ~"/").
+
+render_template(Template,Params) ->
+    TemplateBase = ~"src/templates/",
+    TemplateBinary = list_to_binary(Template),
+    TemplatePath = <<TemplateBase/binary, TemplateBinary/binary>>,
+    {ok, Bin} = file:read_file(TemplatePath),
+    Body = bbmustache:render(Bin, Params),
+    Body.
