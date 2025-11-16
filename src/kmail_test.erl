4edd92e2fa116737a2ffe4f51b7a3d1f7272b7c1
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Prepare to make repo a server

--- src/kmail_test.erl
+++ src/kmail_test.erl
@@ -1,10 +1,12 @@
--include_lib("eunit/include/eunit.hrl").
--module(kmail_test).
+ -include_lib("eunit/include/eunit.hrl").
+ -module(kmail_test).
 
-'when storing a value, it can be retrieved later   _test'() ->
-    ok = repo:store("Hey", "There"),
-    {ok, Value} = repo:retrieve("Hey"),
-    "There" = Value.
+ 'when storing a value, it can be retrieved later   _test'() ->
+    {ok, Repo} = repo:start(),
+    ok = repo:store(Repo, "Hey", "There"),
+    {ok, Value} = repo:retrieve(Repo, "Hey"),
+     "There" = Value.
 
-'when trying to retrieve a value that doesn\'t exist, we get an error   _test'() ->
-    ?assertEqual({error, notfound}, repo:retrieve("nonexistent key")).
+ 'when trying to retrieve a value that doesn\'t exist, we get an error   _test'() ->
+    {ok, Repo} = repo:start(),
+    ?assertEqual({error, notfound}, repo:retrieve(Repo, "nonexistent key")).
--- src/repo.erl
+++ src/repo.erl
@@ -1,21 +1,21 @@
 -module(repo).
 
--export([store/2]).
--export([retrieve/1]).
+-export([store/3]).
+-export([retrieve/2]).
+-export([start/0]).
 
-store(Key, Value) ->
+start() ->
+    {ok, 1}.
+
+store(_, Key, Value) ->
     {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087),
-    MyBucket = <<"kmails">>,
+    MyBucket = ~"kmails",
     Object = riakc_obj:new(MyBucket, term_to_binary(Key), Value),
     riakc_pb_socket:put(Pid, Object).
 
-retrieve(Key) ->
+retrieve(_, Key) ->
     {ok, Pid} = riakc_pb_socket:start("127.0.0.1", 8087),
-    MyBucket = <<"kmails">>,
-    {ok, Fetched} = riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)),
-    Binary = riakc_obj:get_value(Fetched),
-    Value = binary_to_term(Binary),
-    {ok, Value}.
+    MyBucket = ~"kmails",
     case riakc_pb_socket:get(Pid, MyBucket, term_to_binary(Key)) of
         {ok, Fetched} ->
             Binary = riakc_obj:get_value(Fetched),
