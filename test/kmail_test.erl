12025fef73988bc3ecebd7a500018c8686fe3f22
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Add setup and teardown to test!

--- test/kmail_test.erl
+++ test/kmail_test.erl
@@ -1,13 +1,29 @@
 -include_lib("eunit/include/eunit.hrl").
 -module(kmail_test).
 
-'when storing a value, it can be retrieved later   _test'() ->
+setup() ->
     {ok, Repo} = repo:start_link(~"kmail_test"),
-    ok = repo:store(Repo, "Hey", "There"),
-    {ok, Value} = repo:retrieve(Repo, "Hey"),
-    ?assertEqual("There", Value).
+    Repo.
 
-'when trying to retrieve a value that doesn\'t exist, we get an error   _test'() ->
-    {ok, Repo} = repo:start_link(~"kmail_test"),
-    Result = repo:retrieve(Repo, "nonexistent key"),
-    ?assertEqual({error, notfound}, Result).
+teardown(_Repo) ->
+    {ok, Conn} = riakc_pb_socket:start("127.0.0.1", 8087),
+    {ok, Keys} = riakc_pb_socket:list_keys(Conn, ~"kmail_test"),
+    lists:foreach(fun (Key) ->
+        ok = riakc_pb_socket:delete(Conn, ~"kmail_test", Key)
+    end, Keys),
+    ok.
+
+repo_test_() ->
+    {setup, fun setup/0, fun teardown/1, fun (Repo) ->
+        [
+            {"when storing a value, it can be retrieved later", fun () ->
+                ok = repo:store(Repo, "Hey", "There"),
+                {ok, Value} = repo:retrieve(Repo, "Hey"),
+                ?assertEqual("There", Value)
+            end},
+            {"when trying to retrieve a value that doesn't exist, we get an error", fun () ->
+                Result = repo:retrieve(Repo, "nonexistent key"),
+                ?assertEqual({error, notfound}, Result)
+            end}
+        ]
+    end}.
