c2afcff0d9a4f8f5a08632ad3b3e49195b161f01
Karl Eklund <localpart@gmail.com>
2025-11-16 14:00:00 +0100

    Configure release

---
+++ test/kmail_test.erl
@@ -0,0 +1,12 @@
+-include_lib("eunit/include/eunit.hrl").
+-module(kmail_test).
+
+'when storing a value, it can be retrieved later   _test'() ->
+    {ok, Repo} = repo:start(),
+    ok = repo:store(Repo, "Hey", "There"),
+    {ok, Value} = repo:retrieve(Repo, "Hey"),
+    "There" = Value.
+
+'when trying to retrieve a value that doesn\'t exist, we get an error   _test'() ->
+    {ok, Repo} = repo:start(),
+    ?assertEqual({error, notfound}, repo:retrieve(Repo, "nonexistent key")).
